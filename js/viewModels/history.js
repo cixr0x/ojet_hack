e62a91b6460bb297b2b653095e33bd7095f480cb
Diana Duenas <diana-jp0542@hotmail.com>
2017-02-11 10:58:05 -0600

    SPM-000 Arreglos en el CSS

--- js/viewModels/history.js
+++ js/viewModels/history.js
@@ -8,7 +8,7 @@
  * history module
  */
 define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojtrain', 'ojs/ojbutton', 'ojs/ojtabs', 
-    'ojs/ojconveyorbelt', 'promise', 'ojs/ojlistview', 'ojs/ojarraytabledatasource', 'ojs/ojpopup', 'ojs/ojdialog', 'ojs/ojinputtext'
+    'ojs/ojconveyorbelt', 'promise', 'ojs/ojlistview', 'ojs/ojarraytabledatasource', 'ojs/ojpopup', 'ojs/ojdialog', 'ojs/ojinputtext', 'ojs/ojgauge'
 ], function (oj, ko, $) {
     /**
      * The view model for the main content view template
@@ -16,13 +16,22 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojtrain', 'oj
     function historyContentViewModel() {
         var self = this;        
         var id = localStorage.getItem("detailVal");
+        
+        var typeLabel = {};
+        typeLabel["subscription_plan"] = "Subscription Plan";
+        typeLabel["oppty"] = "Opportunity";
+        typeLabel["invoice"] = "Invoice";
                      
         self.historyTitle = ko.observable("Subscription Plan 1000198");
         self.currentStepValue = ko.observable('stp3');
         self.stepArray = ko.observableArray([]);
         self.items = ko.observableArray([{"id":1,"step":"Store","date":"1-Jan-2016 18:00","status":"success","description":"Order sent to CPQ"}]);
         self.dataSource = new oj.ArrayTableDataSource(self.items, {idAttribute: "id"});
-        
+        self.isFavorite = ko.observable(1);
+        self.itemNumber = 1;
+        self.toggleFavorite = function(){
+            self.isFavorite(self.isFavorite() == 1? 0 : 1);
+        };
         
         self.updateSteps=function(){             
             $.getJSON("js/historyDatasource.json", function(data) {
@@ -48,12 +57,15 @@ define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojtrain', 'oj
             $.getJSON("js/mainDatasource.json", function(data) {         
                 var a = [];
                 var name;
+                var count = 0 ;
                 data.forEach(function(item,index){
                     if(item["id"]==id){                         
                         for(var item2 in item)
-                            a.push({"value":item[item2],"label":item2}); 
-                        name=item["type"]+" "+item["id"];
+                            a.push({"value":item2 == 'type'? typeLabel[item[item2]]:item[item2],"label":item2}); 
+                        name=typeLabel[item["type"]]+" "+item["id"];
+                        self.itemNumber = count;
                     }
+                    count++;
                 });
                     self.attributes(a); 
                     self.historyTitle(name);
