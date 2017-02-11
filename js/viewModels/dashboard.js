d3260d01d841eca3aed6e60aa6cc4bc6af759367
Gustavo Leyva <gustavo.leyva@oracle.com>
2017-02-10 22:28:00 -0600

    Gustavo Leyva. Updated search functionality.

--- js/viewModels/dashboard.js
+++ js/viewModels/dashboard.js
@@ -11,71 +11,17 @@ define(['ojs/ojcore', 'knockout', 'jquery'],
     function DashboardViewModel() {
       var self = this;
       self.currentStepValue = ko.observable('stp1');
-		this.stepArray = 
-		  ko.observableArray(
-			  [{label:'Step One', id:'stp1'},
-				 {label:'Step Two', id:'stp2'},
-				 {label:'Step Three', id:'stp3'},
-				 {label:'Step Four', id:'stp4'}, 
-				 {label:'Step Five', id:'stp5'}]);
-       this.currentStepValueText = function() {
+        this.stepArray = 
+          ko.observableArray(
+                  [{label:'Step One', id:'stp1'},
+                         {label:'Step Two', id:'stp2'},
+                         {label:'Step Three', id:'stp3'},
+                         {label:'Step Four', id:'stp4'}, 
+                         {label:'Step Five', id:'stp5'}]);
+       self.currentStepValueText = function() {
 		return ($("#train").ojTrain("getStep", self.currentStepValue())).label;
 	};	                       
-      // Below are a subset of the ViewModel methods invoked by the ojModule binding
-      // Please reference the ojModule jsDoc for additionaly available methods.
-
-      /**
-       * Optional ViewModel method invoked when this ViewModel is about to be
-       * used for the View transition.  The application can put data fetch logic
-       * here that can return a Promise which will delay the handleAttached function
-       * call below until the Promise is resolved.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @return {Promise|undefined} - If the callback returns a Promise, the next phase (attaching DOM) will be delayed until
-       * the promise is resolved
-       */
-      self.handleActivated = function(info) {
-        // Implement if needed
-      };
-
-      /**
-       * Optional ViewModel method invoked after the View is inserted into the
-       * document DOM.  The application can put logic that requires the DOM being
-       * attached here.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @param {boolean} info.fromCache - A boolean indicating whether the module was retrieved from cache.
-       */
-      self.handleAttached = function(info) {
-        // Implement if needed
-      };
-
-
-      /**
-       * Optional ViewModel method invoked after the bindings are applied on this View. 
-       * If the current View is retrieved from cache, the bindings will not be re-applied
-       * and this callback will not be invoked.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       */
-      self.handleBindingsApplied = function(info) {
-        // Implement if needed
-      };
-
-      /*
-       * Optional ViewModel method invoked after the View is removed from the
-       * document DOM.
-       * @param {Object} info - An object with the following key-value pairs:
-       * @param {Node} info.element - DOM element or where the binding is attached. This may be a 'virtual' element (comment node).
-       * @param {Function} info.valueAccessor - The binding's value accessor.
-       * @param {Array} info.cachedNodes - An Array containing cached nodes for the View if the cache is enabled.
-       */
-      self.handleDetached = function(info) {
-        // Implement if needed
-      };
+      
     }
 
     /*
